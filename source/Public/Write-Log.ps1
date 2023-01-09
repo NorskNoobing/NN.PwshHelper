07534cb7dd8c0cd60a2da17b0490e41028bb7215
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2023-01-09 16:13:38 +0100

    Rewrite Write-Log.ps1
    * Changed $Message inputtype from [string] to any to allow outputting from other functions into log to fix #2
    * Changed date format to "yy-MM-dd HH:mm:ss" to fix #1
    * Added option for pipeline input for $Message

--- source/Public/Write-Log.ps1
+++ source/Public/Write-Log.ps1
@@ -1,13 +1,23 @@
 function Write-Log {
     param (
-        [Parameter(Mandatory)][string]$Message,
+        [Parameter(Mandatory,ValueFromPipeline,Position=0)]$Message,
         [Parameter(Mandatory)][string]$Path
     )
     $logDir = $Path.Substring(0, $Path.lastIndexOf('\'))
     if (!(Test-Path $logDir)) {
-        New-Item -ItemType Directory $logDir | Out-Null
+        $null = New-Item -ItemType Directory $logDir
     }
 
-    $timeStamp = Get-Date -Format "dd/MM/yy hh:mm:ss"
-    Add-Content -Path $Path -Value "$timeStamp $message"
+    $timeStamp = Get-Date -Format "yy-MM-dd HH:mm:ss"
+
+
+    if ($Message.gettype().Name -eq "String") {
+        Out-File -Path $Path -Append -InputObject "$timeStamp $message"
+    } else {
+        Out-File -Path $Path -Append -InputObject @"
+$timeStamp
+-----------------
+"@
+        Out-File -Path $Path -Append -InputObject $Message
+    }
 }
